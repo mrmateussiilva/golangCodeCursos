aa8ed6988ebf0d49f78fcdcb36f7bff830e684f1
mrmateussiilva <mateuspython3010@gmail.com>
2025-02-06 21:27:24 -0300

    add ultima aula map aninhado

--- ArraySliceMap/mapaninhado/mapaninhado.go
+++ ArraySliceMap/mapaninhado/mapaninhado.go
@@ -3,6 +3,8 @@ package main
 import "fmt"
 
 func main() {
+
+	//                   CHAVE VALOR CHAVE VALOR
 	funcsPorLetra := map[string]map[string]float64{
 		"A": {
 			"Amanda":   1556.00,
@@ -13,13 +15,15 @@ func main() {
 			"Maicon": 9888.00,
 		},
 	}
+	//delete(funcsPorLetra, "A") // DELETA O MAP
 
 	for letra := range funcsPorLetra {
 		fmt.Println(letra)
 		for k, v := range funcsPorLetra[letra] {
 			fmt.Println("\t", k, v)
 		}
-		//fmt.Println(letra)
+		fmt.Println(letra)
 	}
-	estoqueMercado :=
+	//var funcs map[string]map[string]string // funciona assim
+
 }
